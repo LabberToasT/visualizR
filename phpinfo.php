597a3d79227ac08ed4d11f67c51b57d6bb0e6371
Agnes Gresset <agnes.gresset@hotmail.fr>
2018-05-15 18:54:57 +0200

    database connection

---
+++ phpinfo.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "visualizr";
+
+// Create connection
+$connection = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+
+?>
